1573000e9f31732f7463cc4c33167db284f525a7
niels <nm@9la.dk>
2016-09-17 17:45:47 +0200

    Cleaned up webpack configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,21 @@
-/**
- * Webpack config for development
- */
-module.exports = require('./webpack.make')({
-  BUILD: false,
-  TEST: false
-});
+var webpack = require('webpack')
+
+var options = {
+  entry: "./index.js",
+  output: {
+    path: __dirname + '/static',
+    filename: "app.js"
+  },
+  devtool: "source-map",
+  module: {
+    preLoaders: [
+      { test: /\.json$/, loader: 'json'}
+    ],
+    loaders: [
+      { test: /\.css$/, loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]' },
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  }
+};
+
+module.exports = options;
